f825b77c8b83025be769e4b92e20b912f67add51
t201999 <t201999@gs.hs.ntnu.edu.tw>
2026-03-12 17:55:19 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,11 @@
+code
+Ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000
+  }
 })
